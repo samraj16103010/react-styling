418cfdcd39a05c70eebde095e540863af96028f2
MRINALINI <demoprojects123@gmail.com>
2021-06-15 09:27:50 +0530

    message

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from'./CourseInput.module.css';
+//import styled from 'styled-components';
+
+
+/* const FormControl = styled.div`
+    
+      margin: 0.5rem 0;
+  
+     & label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+      color:${props => (props.invalid?'red':'black')}
+    }
+
+    & input {
+      display: block;
+      width: 100%;
+      border: 1px solid ${props => (props.invalid?'red':'#ccc')};
+      background:${props => (props.invalid?'#ffd7d7':'transparent')};
+      font: inherit;
+      line-height: 1.5rem;
+      padding: 0 0.25rem;
+    }
+
+    & input:focus {
+      outline: none;
+      background: #fad0ec;
+      border-color: #8b005d;
+    }
+
+    .error-message{
+      font-weight: bold;
+      color: red;
+    }
+
+`; */
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,11 +61,14 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid':''}`}>
+      {/*<FormControl className = {!isValid && 'invalid'}> One way of doing this*/ }
+      {/* Another way of doing this is by using dynamic props */}
+      {/* <FormControl invalid = {!isValid}>  This syntax for dynamic props*/}
+      <div className = {`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      {!isValid && <div><p className = 'error-message'>Please Enter Some Value!!</p></div>}
+      {!isValid && <div><p className = {styles['error-message']}>Please Enter Some Value!!</p></div>}
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Button.css';
-import styled from 'styled-components';
+import styles from './Button.module.css';
+//import styled from 'styled-components';
 
-const Button = styled.button`
-
+/* const Button = styled.button`
+  width:100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -12,7 +12,11 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+  
 
+  @media(min-width:768px){
+      width:auto;
+   }
 
 &:focus {
   outline: none;
@@ -25,14 +29,14 @@ const Button = styled.button`
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
 }
 
-`;
+`; */
 
-/*const Button = props => {
+const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
-};*/
+};
 
 export default Button;
